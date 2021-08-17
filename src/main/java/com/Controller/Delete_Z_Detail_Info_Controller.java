83f450760be12b42978aeba0ee03fade0b63604f
smhrd <smhrd@DESKTOP-6IHAUI2>
2021-08-17 19:09:49 +0900

    박종학천재

---
+++ src/main/java/com/Controller/Delete_Z_Detail_Info_Controller.java
@@ -0,0 +1,45 @@
+package com.Controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.Model.Z_Detail_info_DAO;
+import com.Model.Z_Detail_info_DTO;
+
+public class Delete_Z_Detail_Info_Controller implements Command {
+
+	@Override
+	public void command(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("EUC-KR");
+
+		int numbering = Integer.parseInt(request.getParameter("numbering"));
+		int req = Integer.parseInt("req");
+
+		Z_Detail_info_DTO dto = new Z_Detail_info_DTO(numbering);
+		Z_Detail_info_DAO dao = new Z_Detail_info_DAO();
+	    int rtn = dao.delete_Z_Detail_Info(dto);
+
+		if (req == 0) {
+			if (rtn > 0 ) {
+				response.sendRedirect("");
+			} else {
+				response.sendRedirect("");
+			}
+		} else {
+			PrintWriter out = response.getWriter();
+			if (rtn > 0 ) {
+				out.print("1");
+			} else {
+				out.print("0");
+			}
+		}
+
+
+	}
+
+}
--- src/main/java/com/Controller/GetAll_Z_Detail_Info_Controller.java
+++ src/main/java/com/Controller/GetAll_Z_Detail_Info_Controller.java
@@ -1,5 +1,7 @@
 package com.Controller;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
@@ -8,8 +10,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.Model.K_Detail_Info_DAO;
-import com.Model.K_Detail_Info_DTO;
 import com.Model.Z_Detail_info_DAO;
 import com.Model.Z_Detail_info_DTO;
 
@@ -28,17 +28,40 @@ public class GetAll_Z_Detail_Info_Controller implements Command {
 		ArrayList<Z_Detail_info_DTO> resultDTO = dao.All_Z_Detail_info();
 
 		if (req == 0) {
-			if (resultDTO.size() > 0 ) {
+			if (resultDTO.size() > 0) {
 				response.sendRedirect("");
 			} else {
 				response.sendRedirect("");
 			}
 		} else {
 			PrintWriter out = response.getWriter();
-			if (resultDTO.size() > 0 ) {
-				out.print("1");
+			if (resultDTO.size() > 0) {
+
+				JSONObject rtn = new JSONObject();
+				JSONArray req_array = new JSONArray();
+				
+				for (int i = 0; i < resultDTO.size(); i++) {
+					JSONObject data = new JSONObject();
+					data.put("numbering", resultDTO.get(i).getNumbering());
+					data.put("salinity", resultDTO.get(i).getNumbering());
+					data.put("indoor_temp", resultDTO.get(i).getNumbering());
+					data.put("indoor_humid", resultDTO.get(i).getNumbering());
+					data.put("water_temp", resultDTO.get(i).getNumbering());
+					data.put("wire_temp", resultDTO.get(i).getNumbering());
+					data.put("water_high", resultDTO.get(i).getNumbering());
+					data.put("place_size", resultDTO.get(i).getNumbering());
+					data.put("pump_move", resultDTO.get(i).getNumbering());
+					
+					req_array.add(data);
+				}
+				
+				
+				
+				rtn.put("data", req_array);
+			
+				out.print(rtn.toJSONString());
 			} else {
-				out.print("0");
+				out.print("False");
 			}
 		}
 
