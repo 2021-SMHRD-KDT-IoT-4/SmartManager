b3d40d200ae2e68dd019a347427db3b81e9a81f0
TTOCHI <jhpark1546@gmail.com>
2021-08-25 13:05:28 +0900

    GetAutoController 반환 respones 수정

--- src/main/java/com/Controller/GetAuto_RunningController.java
+++ src/main/java/com/Controller/GetAuto_RunningController.java
@@ -1,11 +1,14 @@
 package com.Controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import org.json.simple.JSONObject;
 
 import com.Model.Auto_Running_DAO;
 import com.Model.Auto_Running_DTO;
@@ -22,13 +25,26 @@ public class GetAuto_RunningController implements Command {
 		Auto_Running_DTO resultdto = dao.Get_Auto_Running(dto);
 
 		/* Domestic_AnimalsDTO[] domestic_animalsDTO[] = dto.get(dto.length); */
-
+		PrintWriter out = response.getWriter();
+		
 		if (resultdto != null) {
-			HttpSession session = request.getSession();
-			session.setAttribute("Auto_Running", dto);
+			
+			JSONObject data = new JSONObject();
+			data.put("numbering", resultdto.getNumbering());
+			data.put("fan_run", resultdto.getFan_run());
+			data.put("pump_run", resultdto.getPump_run());
+			data.put("wire_run", resultdto.getWire_run());
+			data.put("light_run", resultdto.getLight_run());
+			data.put("stalinity_run", resultdto.getStalinity_run());
+			data.put("waterhigh_run", resultdto.getWaterhigh_run());
+			
+			out.print(data.toJSONString());
+		
+		}else {
+			out.print("0");
 		}
 		
-		response.sendRedirect("jsp");
+		
 	}
 
 }
